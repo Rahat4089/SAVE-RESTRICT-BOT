000c7d45bc698320be1cbf09ca39d6ac508b09b7
RAHAT <83128928+Rahat4089@users.noreply.github.com>
2026-02-20 14:44:31 +0600

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,14 +1,1 @@
-# cantarella
-# Don't Remove Credit
-# Telegram Channel @cantarellabots
 
-
-# cantarella
-# Don't Remove Credit
-# Telegram Channel @cantarellabots
-
-#from database.DB import db
-
-# cantarella
-# Don't Remove Credit
-# Telegram Channel @cantarellabots
