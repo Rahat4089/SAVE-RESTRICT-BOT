ab260023ba82922348f514e2619c5f8ff633fc58
RAHAT <83128928+Rahat4089@users.noreply.github.com>
2026-03-18 01:22:12 +0600

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ import os
 # Telegram Bot Credentials
 # ==============================
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8671678677:AAEZVEwmyOI5NR35acta8dyUhKerA5z7hDg")
+API_ID = int(os.environ.get("API_ID", "23933044"))
+API_HASH = os.environ.get("API_HASH", "6df11147cbec7d62a323f0f498c8c03a")
 
 
 # ==============================
@@ -21,14 +21,14 @@ API_HASH = os.environ.get("API_HASH", "")
 # ==============================
 
 # Add admin user IDs separated by commas in environment variables
-ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "").split(",") if admin]
+ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "7125341830").split(",") if admin]
 
 
 # ==============================
 # Database Configuration
 # ==============================
 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://animepahe:animepahe@animepahe.o8zgy.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 
 
@@ -37,7 +37,7 @@ DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 # ==============================
 
 # Replace with your Telegram log channel ID (example: -1001234567890)
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003735141807"))
 
 
 # ==============================
